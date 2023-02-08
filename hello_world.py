5390f41fa28041a91c26458baf4973f0e303fdda
whizzystainz <whizdom2654@gmail.com>
2023-02-08 09:48:41 +0100

    I added a new python file

---
+++ hello_world.py
@@ -0,0 +1,43 @@
+message = "hello world!"
+print(message)
+message = "happy to run my first python code!"
+print(message)
+ 
+name = "wisdom dangiwa"
+print(name)
+age = 27
+status = "single"
+print(status)
+
+
+First_name = "lionel"
+Last_name = "messi"
+Full_name = "{lionel} {messi}"
+print(Full_name)
+
+
+
+
+
+
+message = "BEST PROGRAMMER"
+print(message)
+First_name = "wisdom"
+Last_name = "dangiwa"
+Full_name = "{wisdom} {dangiwa}"
+print(Full_name)
+Remarks = "it only gets better from here!"
+print(Remarks)
+
+
+import time
+name = "loading"
+while True:
+    print(name)
+    time.sleep(0.5)
+    time.sleep(0.5)
+    print(name)
+    
+
+
+   
